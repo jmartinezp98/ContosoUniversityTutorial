3ad87ed25057662f9c6ac2717979b239adcf814f
jmartinezp98 <301710151@bis.ute.edu.mx>
2019-01-11 18:20:06 -0600

    Se crea clase ContosoDbContext, se añade cadena de conexión, método OnModelCreating y el MySqlEFConfiguration

---
+++ Practica/BusinessEntity/ContosoDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessEntity
+{
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+
+    public class ContosoDbContext : DbContext
+    {
+        public ContosoDbContext() : base("ContosoDbContext")
+        {
+            //constructor vacio
+        }
+
+        //tablas
+
+        //tablas
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
